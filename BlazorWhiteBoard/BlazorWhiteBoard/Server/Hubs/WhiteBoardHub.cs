d60a6e22a1ae1485fdc998717b3b30807e3c0226
Miguel teheran <miguelteheran@gmail.com>
2020-07-21 21:49:45 -0500

    adding whte board example base code

---
+++ BlazorWhiteBoard/BlazorWhiteBoard/Server/Hubs/WhiteBoardHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorWhiteBoard.Server.Hubs
+{
+    public class WhiteBoardHub : Hub
+    {
+        public Task Draw(int prevX, int prevY, int currentX, int currentY, string color)
+        {
+            return Clients.Others.SendAsync("draw", prevX, prevY, currentX, currentY, color);
+        }
+    }
+}
